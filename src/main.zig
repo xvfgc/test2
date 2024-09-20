3653ee0990d52715b80e00b7e8c98eaafef8b1e0
x.x <38306136+xvfgc@users.noreply.github.com>
2024-09-20 16:31:13 -0300

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,134 +1,129 @@
 const std = @import("std");
 
-// const c = @cImport({
-//     @cInclude("p2p_api.h");
-// });
+pub const packing = enum(c_int) {
+    rgb24_be,
+    rgb24_le,
+    rgb24,
 
-pub const p2p_packing = enum(c_int) {
-    p2p_rgb24_be,
-    p2p_rgb24_le,
-    p2p_rgb24,
+    argb32_be,
+    argb32_le,
+    argb32,
 
-    p2p_argb32_be,
-    p2p_argb32_le,
-    p2p_argb32,
+    ayuv_be,
+    ayuv_le,
+    ayuv,
 
-    p2p_ayuv_be,
-    p2p_ayuv_le,
-    p2p_ayuv,
+    rgb48_be,
+    rgb48_le,
+    rgb48,
 
-    p2p_rgb48_be,
-    p2p_rgb48_le,
-    p2p_rgb48,
+    argb64_be,
+    argb64_le,
+    argb64,
 
-    p2p_argb64_be,
-    p2p_argb64_le,
-    p2p_argb64,
+    rgb30_be,
+    rgb30_le,
+    rgb30,
 
-    p2p_rgb30_be,
-    p2p_rgb30_le,
-    p2p_rgb30,
+    y410_be,
+    y410_le,
+    y410,
 
-    p2p_y410_be,
-    p2p_y410_le,
-    p2p_y410,
+    y412_be,
+    y412_le,
+    y412,
 
-    p2p_y412_be,
-    p2p_y412_le,
-    p2p_y412,
+    y416_be,
+    y416_le,
+    y416,
 
-    p2p_y416_be,
-    p2p_y416_le,
-    p2p_y416,
+    yuy2,
 
-    p2p_yuy2,
+    uyvy,
 
-    p2p_uyvy,
+    y210_be,
+    y210_le,
+    y210,
 
-    p2p_y210_be,
-    p2p_y210_le,
-    p2p_y210,
+    y212_be,
+    y212_le,
+    y212,
 
-    p2p_y212_be,
-    p2p_y212_le,
-    p2p_y212,
+    y216_be,
+    y216_le,
+    y216,
 
-    p2p_y216_be,
-    p2p_y216_le,
-    p2p_y216,
+    v210_be,
+    v210_le,
+    v210,
 
-    p2p_v210_be,
-    p2p_v210_le,
-    p2p_v210,
+    v216_be,
+    v216_le,
+    v216,
 
-    p2p_v216_be,
-    p2p_v216_le,
-    p2p_v216,
+    nv12_be,
+    nv12_le,
+    nv12,
 
-    p2p_nv12_be,
-    p2p_nv12_le,
-    p2p_nv12,
+    nv16_be,
+    nv16_le,
+    nv16,
 
-    p2p_nv16_be,
-    p2p_nv16_le,
-    p2p_nv16,
+    p010_be,
+    p010_le,
+    p010,
 
-    p2p_p010_be,
-    p2p_p010_le,
-    p2p_p010,
+    p012_be,
+    p012_le,
+    p012,
 
-    p2p_p012_be,
-    p2p_p012_le,
-    p2p_p012,
+    p016_be,
+    p016_le,
+    p016,
 
-    p2p_p016_be,
-    p2p_p016_le,
-    p2p_p016,
+    p210_be,
+    p210_le,
+    p210,
 
-    p2p_p210_be,
-    p2p_p210_le,
-    p2p_p210,
+    p212_be,
+    p212_le,
+    p212,
 
-    p2p_p212_be,
-    p2p_p212_le,
-    p2p_p212,
+    p216_be,
+    p216_le,
+    p216,
 
-    p2p_p216_be,
-    p2p_p216_le,
-    p2p_p216,
+    rgba32_be,
+    rgba32_le,
+    rgba32,
 
-    p2p_rgba32_be,
-    p2p_rgba32_le,
-    p2p_rgba32,
+    rgba64_be,
+    rgba64_le,
+    rgba64,
 
-    p2p_rgba64_be,
-    p2p_rgba64_le,
-    p2p_rgba64,
+    abgr64_be,
+    abgr64_le,
+    abgr64,
 
-    p2p_abgr64_be,
-    p2p_abgr64_le,
-    p2p_abgr64,
+    bgr48_be,
+    bgr48_le,
+    bgr48,
 
-    p2p_bgr48_be,
-    p2p_bgr48_le,
-    p2p_bgr48,
+    bgra64_be,
+    bgra64_le,
+    bgra64,
 
-    p2p_bgra64_be,
-    p2p_bgra64_le,
-    p2p_bgra64,
-
-    p2p_packing_max,
+    packing_max,
 };
 
-const ptrdiff_t = c_longlong;
-pub const p2p_buffer_param = extern struct {
-    src: [4]?*const anyopaque = @import("std").mem.zeroes([4]?*const anyopaque),
-    dst: [4]?*anyopaque = @import("std").mem.zeroes([4]?*anyopaque),
-    src_stride: [4]ptrdiff_t = @import("std").mem.zeroes([4]ptrdiff_t),
-    dst_stride: [4]ptrdiff_t = @import("std").mem.zeroes([4]ptrdiff_t),
-    width: c_uint = @import("std").mem.zeroes(c_uint),
-    height: c_uint = @import("std").mem.zeroes(c_uint),
-    packing: p2p_packing = @import("std").mem.zeroes(p2p_packing),
+pub const buffer_param = extern struct {
+    src: [4]?*const anyopaque,
+    dst: [4]?*anyopaque,
+    src_stride: [4]c_longlong,
+    dst_stride: [4]c_longlong,
+    width: c_uint,
+    height: c_uint,
+    packing: packing,
 };
 
 pub const Flags = enum(c_ulong) {
@@ -136,10 +131,12 @@ pub const Flags = enum(c_ulong) {
     ALPHA_SET_ONE = @as(c_ulong, 1) << 1,
 };
 
-pub const p2p_unpack_func = ?*const fn (?*const anyopaque, [*c]const ?*anyopaque, c_uint, c_uint) callconv(.C) void;
-pub const p2p_pack_func = ?*const fn ([*c]const ?*const anyopaque, ?*anyopaque, c_uint, c_uint) callconv(.C) void;
-pub extern fn p2p_select_unpack_func(packing: p2p_packing) p2p_unpack_func;
-pub extern fn p2p_select_pack_func(packing: p2p_packing) p2p_pack_func;
-pub extern fn p2p_select_pack_func_ex(packing: p2p_packing, alpha_one_fill: c_int) p2p_pack_func;
-pub extern fn p2p_unpack_frame(param: [*c]const p2p_buffer_param, flags: c_ulong) void;
-pub extern fn p2p_pack_frame(param: [*c]const p2p_buffer_param, flags: c_ulong) void;
+pub const unpack_func = ?*const fn (?*const anyopaque, [*c]const ?*anyopaque, c_uint, c_uint) callconv(.C) void;
+pub const pack_func = ?*const fn ([*c]const ?*const anyopaque, ?*anyopaque, c_uint, c_uint) callconv(.C) void;
+
+pub extern fn select_unpack_func(packing: packing) unpack_func;
+pub extern fn select_pack_func(packing: packing) pack_func;
+pub extern fn select_pack_func_ex(packing: packing, alpha_one_fill: c_int) pack_func;
+
+pub extern fn unpack_frame(param: [*c]const buffer_param, flags: Flags) void;
+pub extern fn pack_frame(param: [*c]const buffer_param, flags: Flags) void;
